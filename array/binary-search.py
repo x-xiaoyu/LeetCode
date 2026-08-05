2b97f95c9175ebd2943687206bc8426dec37a409
Susie X. <119790879+x-xiaoyu@users.noreply.github.com>
2026-08-04 21:00:15 -0400

    LC-binary-search [Runtime: 44 ms; Memory: 20.5 MB]

--- array/binary-search.py
+++ array/binary-search.py
@@ -3,14 +3,14 @@ class Solution:
         left = 0
         right = len(nums) - 1
 
-        while left < right:
-            # mid[left] < target
-            # mid[right] >= target
-            mid = (right + left) // 2 
-            if nums[mid] < target:
-                left = mid + 1 # range [mid + 1, right]
-            else:
-                right = mid
-        
-        return left
+        while left <= right:
+            mid = left + (right - left)//2
 
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid -1 
+            else:
+                return mid
+            
+        return -1
