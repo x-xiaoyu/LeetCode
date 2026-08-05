a4e4eb6f9aa77eb810e4fbb0027aca63a99e07a6
Susie X. <119790879+x-xiaoyu@users.noreply.github.com>
2026-08-05 14:52:38 -0400

    LC-using-a-robot-to-print-the-lexicographically-smallest-string [Runtime: N/A; Memory: N/A]

--- stack/using-a-robot-to-print-the-lexicographically-smallest-string.py
+++ stack/using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -2,27 +2,23 @@ class Solution:
     def robotWithString(self, s: str) -> str:
         n = len(s)
 
-        # min_suffix[i] 表示从 s[i] 到最后的最小字符
-        min_suffix = list(s)
+        min_stuff = list(s)
 
-        for i in range(n - 2, -1, -1):
-            min_suffix[i] = min(min_suffix[i], min_suffix[i + 1])
-
+        for i in range(n-2, -1, -1):
+            min_stuff[i] = min(min_stuff[i], min_stuff[i + 1])
+        
         t = []
-        p = []
+        paper = []
 
-        for i, char in enumerate(s):
-            # s 的第一个字符进入 t 的末尾
+        for i , char in enumerate(s):
             t.append(char)
 
-            # s 剩余部分的最小字符
             if i + 1 < n:
-                remaining_min = min_suffix[i + 1]
+                remain_num = min_stuff[i + 1]
             else:
-                remaining_min = "{"
-
-            # t只能从最后一个字符取
-            while t and t[-1] <= remaining_min:
-                p.append(t.pop())
-
-        return "".join(p)
+                remain_num = "{"
+            
+            while t and t[-1] <= remain_num:
+                paper.append(t.pop())
+        return "".join(paper)
+ 
