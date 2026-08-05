ac9ec8d17f65b5bb30c8e7bfd42130692bd9fd81
Susie X. <119790879+x-xiaoyu@users.noreply.github.com>
2026-08-05 13:46:23 -0400

    LC-longest-substring-of-all-vowels-in-order [Runtime: N/A; Memory: N/A]

--- sliding-window/longest-substring-of-all-vowels-in-order.py
+++ sliding-window/longest-substring-of-all-vowels-in-order.py
@@ -1,20 +1,16 @@
 class Solution:
     def longestBeautifulSubstring(self, word: str) -> int:
         left = 0
-        answer = 0
         types = 1
+        answer = 0
 
         for right in range(1, len(word)):
             if word[right] < word[right - 1]:
                 left = right
                 types = 1
-            
             elif word[right] > word[right - 1]:
                 types += 1
 
             if types == 5:
                 answer = max(answer, right - left + 1)
-                
         return answer
-            
-        
