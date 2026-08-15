aa00daa88ca312b524d2415f5791ac9f1bb2e5c2
Susie X. <119790879+x-xiaoyu@users.noreply.github.com>
2026-08-15 12:22:29 -0400

    LC-top-k-frequent-elements [Runtime: 46 ms; Memory: 22.9 MB]

--- hash-table/top-k-frequent-elements.py
+++ hash-table/top-k-frequent-elements.py
@@ -1,18 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # 频率 次数 -》hashmap
+        # 第一步：统计每个元素的出现次数
+        cnt = Counter(nums)
+        max_cnt = max(cnt.values())
 
-        count_map = {}
-        for num in nums:
-            count_map[num] = count_map.get(num, 0) + 1
+        # 第二步：把出现次数相同的元素，放到同一个桶中
+        buckets = [[] for _ in range(max_cnt + 1)]  # 也可以用 defaultdict(list)
+        for x, c in cnt.items():
+            buckets[c].append(x)
 
-        fre_bucket = [[] for _ in range(len(nums)+ 1)]
-        for num, freq in count_map.items():
-            fre_bucket[freq].append(num)
-
-        result = []
-        for i in range(len(fre_bucket)):
-            for num in fre_bucket[i]:
-                result.append(num)
-                if (len(result)) == k:
-                    return result
+        # 第三步：倒序遍历 buckets，把出现次数前 k 大的元素加入答案
+        ans = []
+        for bucket in reversed(buckets):
+            ans += bucket
+            # 注意题目保证答案唯一，一定会出现恰好等于 k 的情况
+            if len(ans) == k:
+                return ans
