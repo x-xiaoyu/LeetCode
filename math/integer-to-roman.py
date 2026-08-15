30fe8487a8e87f4914eff1f7fe3159fda94fa29b
Susie X. <119790879+x-xiaoyu@users.noreply.github.com>
2026-08-15 13:58:37 -0400

    LC-integer-to-roman [Runtime: 0 ms; Memory: 19.4 MB]

---
+++ math/integer-to-roman.py
@@ -0,0 +1,10 @@
+R = (
+    ("", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"),  # 个位
+    ("", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"),  # 十位
+    ("", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"),  # 百位
+    ("", "M", "MM", "MMM"),  # 千位
+)
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        return R[3][num // 1000] + R[2][num // 100 % 10] + R[1][num // 10 % 10] + R[0][num % 10]
